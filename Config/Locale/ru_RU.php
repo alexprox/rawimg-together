939f7db3ad3498e07eee23135a8632466511ff31
Alex <alexprox91@gmail.com>
2013-11-05 17:28:07 +0200

    Russian locale added
    text added

---
+++ Config/Locale/ru_RU.php
@@ -0,0 +1,10 @@
+<?php
+    return array(
+        'locale' => 'ru_RU',
+        'home page' => 'Главная',
+        'logout' => 'Выход',
+        'please sign in' => 'Кто там?',
+        'login' => 'Входное имя',
+        'password' => 'Пароль',
+        'sign in' => 'Вход',
+    );
