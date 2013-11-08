caca2ff5738de0ecfa82528cdc76579ce9639008
Alex <alexprox91@gmail.com>
2013-11-08 17:31:56 +0200

    Added not found exception and Errorheaders

--- App/Debug.php
+++ App/Debug.php
@@ -8,6 +8,14 @@
          */
         public function render_exceptions($exception)
         {
+            $status = '503 Service Temporarily Unavailable';
+          
+            if($exception instanceof NotFoundException)
+                $status = '404 Not Found';
+              
+            header($_SERVER["SERVER_PROTOCOL"].' '.$status);
+            header("Status: ".$status);
+            
             echo $exception->getMessage();
             echo '<pre>';
             echo $exception->getTraceAsString();
