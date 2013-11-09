a22f7fabc6d278f33d8fe3432f99fc0d24a89d5f
Alex <alexprox91@gmail.com>
2013-11-09 14:20:54 +0200

    Home page updated (last 4 sketches output)

--- Page/Home.php
+++ Page/Home.php
@@ -7,5 +7,16 @@
         public function action_home()
         {
             $this->view->subview = 'Content/Home';
+            $sql = 'SELECT ImageID as ID, Created '
+                .'    FROM access_users_images '
+                .'   WHERE UserID = :user '
+                .'ORDER BY Created DESC '
+                .'   LIMIT 4;';
+            $images = $this->db()->query($sql)
+                    ->bind('user', $this->user()->id())
+                    ->execute();
+            if(isset($images['ID']))
+                $images = array($images);
+            $this->view->images = $images;
         }
     }
