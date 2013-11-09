25234f7d580243294650d8ebc0aef4e1cda08dc8
Alex <alexprox91@gmail.com>
2013-11-09 15:26:07 +0200

    Route fix /my

--- Config/_Router.php
+++ Config/_Router.php
@@ -3,6 +3,9 @@
         '/' => array('Home', 'home'),
         '/login' => array('User', 'login'),
         '/logout' => array('User', 'logout'),
+        
         '/new' => array('Sketch', 'new'),
         '/get/{imageid}' => array('Sketch', 'get'),
+        '/my' => array('Sketch', 'get_all'),
+        '/my/{page}' => array('Sketch', 'get_all'),
     );
